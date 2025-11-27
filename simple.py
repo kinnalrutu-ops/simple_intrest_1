87ad38416baaecb276c5dc8f70fcea9777d675bc
Rutu <kinnalrutu@gmail.com>
2025-11-27 23:12:46 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -1,15 +1,12 @@
 import sys
-print("Simple and Compound Interest Calculator")
-if len(sys.argv) == 5:
+print("Simple Interest Calculator")
+if len(sys.argv) == 3:
     p = float(sys.argv[1])
     r = float(sys.argv[2])
     t = float(sys.argv[3])
-    n = int(sys.argv[4])
 else:
    p=5000
    r=5
    t=10
-   n=4
-r = r / 100
-simple_interest = p * r * t
+simple_interest = p * r * t/100
 print("Simple Interest:", round(simple_interest, 2))
